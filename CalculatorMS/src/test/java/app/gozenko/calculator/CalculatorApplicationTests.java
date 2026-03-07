7b63f2c1e27f16c237c53202c000fb367528a7b6
Дарья <gozenko.dasha@bk.ru>
2026-03-07 23:28:57 +0400

    initial commit

---
+++ CalculatorMS/src/test/java/app/gozenko/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package app.gozenko.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
