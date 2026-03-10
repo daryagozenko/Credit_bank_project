d97f02373107806633010507ec2f8302e84a98d3
Дарья <gozenko.dasha@bk.ru>
2026-03-11 00:26:54 +0400

    modified class ValidateData, add plugin to lombok and maven

--- CalculatorMS/src/main/java/app/gozenko/Controllers/CalculatorController.java
+++ CalculatorMS/src/main/java/app/gozenko/Controllers/CalculatorController.java
@@ -7,6 +7,7 @@ import app.gozenko.DTO.ScoringDataDto;
 import app.gozenko.Services.LoanOfferService;
 import app.gozenko.Services.PreScoringService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +26,7 @@ public class CalculatorController {
     }
 
     @PostMapping("/offers")
-    public List<LoanOfferDto> calcConditionOfCredit(LoanStatementRequestDto loanState){
+    public List<LoanOfferDto> calcConditionOfCredit(@RequestBody LoanStatementRequestDto loanState){
         preScoringService.preScoring(loanState);
         return loanOfferService.createLoanOffers(
                 loanState.getAmount(),
