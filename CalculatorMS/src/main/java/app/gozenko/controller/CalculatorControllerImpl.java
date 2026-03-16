675f3da0a5dbbeae7caefe87692507b91caa30e2
Дарья <gozenko.dasha@bk.ru>
2026-03-17 00:29:06 +0400

    Add versioning and api response options
    - add api versioning in the controller to make it easier to track changes in the future.
    - add endpoints response display via swagger

--- CalculatorMS/src/main/java/app/gozenko/controller/CalculatorControllerImpl.java
+++ CalculatorMS/src/main/java/app/gozenko/controller/CalculatorControllerImpl.java
@@ -1,18 +1,23 @@
 package app.gozenko.controller;
 
+import app.gozenko.controller.interfaces.CalculatorController;
 import app.gozenko.dto.CreditDto;
 import app.gozenko.dto.LoanOfferDto;
 import app.gozenko.dto.LoanStatementRequestDto;
 import app.gozenko.dto.ScoringDataDto;
-import app.gozenko.controller.interfaces.CalculatorController;
 import app.gozenko.service.interfaces.LoanOfferService;
 import app.gozenko.service.interfaces.PreScoringService;
 import app.gozenko.service.interfaces.ScoringService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,9 +27,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @Slf4j
-@RestController
-@RequestMapping("/calculator")
 @RequiredArgsConstructor
+@RestController
+@RequestMapping("/calculator/api/v1")
 @Tag(name = "CalculatorController")
 public class CalculatorControllerImpl implements CalculatorController {
 
@@ -35,8 +40,20 @@ public class CalculatorControllerImpl implements CalculatorController {
 
     @PostMapping("/offers")
     @Operation(summary = "создание кредитной заявки")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Успешно",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = LoanOfferDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Неверные параметры запроса"
+            )})
     @Override
-    public ResponseEntity<List<LoanOfferDto>> calcConditionOfCredit(@RequestBody LoanStatementRequestDto loanState){
+    public ResponseEntity<List<LoanOfferDto>> calcConditionOfCredit(@RequestBody LoanStatementRequestDto loanState) {
         log.info("Sending a LoanStatementRequest to preScoringService");
         preScoringService.preScoringLoan(loanState);
         log.info("Successful create list of loanOffers");
@@ -48,8 +65,20 @@ public class CalculatorControllerImpl implements CalculatorController {
 
     @PostMapping("/calc")
     @Operation(summary = "расчет полной стоимости кредита и графика платежей")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Успешно",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = CreditDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Неверные параметры запроса"
+            )})
     @Override
-    public ResponseEntity<CreditDto> validateAndCalc(@RequestBody ScoringDataDto scoringData){
+    public ResponseEntity<CreditDto> validateAndCalc(@RequestBody ScoringDataDto scoringData) {
         log.info("Sending a ScoringData to preScoringService");
         preScoringService.preScoringScoreData(scoringData);
         log.info("Successful create credit offer");
