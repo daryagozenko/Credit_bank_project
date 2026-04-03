eac3174bf63e0c28d51913d4c205e06d14fcab24
Дарья <gozenko.dasha@bk.ru>
2026-04-03 10:51:08 +0400

    Add a description of the dto fields in the documentation

--- DealMS/src/main/java/app/gozenko/dto/PassportDto.java
+++ DealMS/src/main/java/app/gozenko/dto/PassportDto.java
@@ -1,5 +1,6 @@
 package app.gozenko.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,13 +12,20 @@ import java.time.LocalDate;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(name = "Паспортные данные")
 public class PassportDto {
 
+    @Schema(description = "Серия паспорта", example = "1212",
+            minLength = 4, maxLength = 4)
     private String series;
 
+    @Schema(description = "Номер паспорта", example = "121212",
+            minLength = 6, maxLength = 6)
     private String number;
 
+    @Schema(description = "Название подразделения", example = "МВД России")
     private String issueBranch;
 
+    @Schema(description = "Название подразделения", example = "МВД России")
     private LocalDate issueDate;
 }
--- DealMS/src/main/java/app/gozenko/dto/StatementStatusHistoryDto.java
+++ DealMS/src/main/java/app/gozenko/dto/StatementStatusHistoryDto.java
@@ -1,6 +1,7 @@
 package app.gozenko.dto;
 import app.gozenko.enums.StatementStatus;
 import app.gozenko.enums.StatusChangeType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,11 +13,15 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(name = "История статусов заявки")
 public class StatementStatusHistoryDto {
 
+    @Schema(description = "Статус заявки", example = "APPROVED")
     private StatementStatus status;
 
+    @Schema(description = "Время установки статуса", example = "2026-02-02")
     private LocalDateTime time;
 
+    @Schema(description = "Способ установки статуса", example = "AUTOMATIC")
     private StatusChangeType changeType;
 }
