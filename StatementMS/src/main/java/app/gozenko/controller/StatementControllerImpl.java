5347e9d5ac896523f1565e4177c989e4f130a518
Дарья <gozenko.dasha@bk.ru>
2026-04-12 22:20:33 +0400

    Add swagger docs in StatementController

--- StatementMS/src/main/java/app/gozenko/controller/StatementControllerImpl.java
+++ StatementMS/src/main/java/app/gozenko/controller/StatementControllerImpl.java
@@ -5,9 +5,14 @@ import app.gozenko.dto.LoanOfferDto;
 import app.gozenko.dto.LoanStatementRequestDto;
 import app.gozenko.service.DealCallingService;
 import app.gozenko.service.interfaces.PreScoringService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +31,26 @@ public class StatementControllerImpl implements StatementController {
 
     @PostMapping("/")
     @Override
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Успешно",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = LoanOfferDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Неверные параметры запроса",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE)
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Внутренняя ошибка сервера",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE)
+            )})
     public ResponseEntity<List<LoanOfferDto>> calcConditionOfCredit(LoanStatementRequestDto loanState) {
         log.info("Input data in calcConditionOfCredit loanState-{}", loanState);
         log.debug("Sending a LoanStatementRequest to preScoringService");
@@ -39,6 +64,26 @@ public class StatementControllerImpl implements StatementController {
 
     @PostMapping("/offer")
     @Override
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Успешно",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = LoanOfferDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Неверные параметры запроса",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE)
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Внутренняя ошибка сервера",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE)
+            )})
     public ResponseEntity<Void> selectLoanOffer(LoanOfferDto loanOffer) {
         log.info("Input data in selectLoanOffer loanOffer-{}", loanOffer);
         dealCallingService.selectLoanOffer(loanOffer);
