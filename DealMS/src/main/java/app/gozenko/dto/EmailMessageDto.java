f90a770f48276475b7c97ed43b56ae87e5abe693
Дарья <gozenko.dasha@bk.ru>
2026-04-27 22:07:19 +0400

    Add full swagger configs and annotations in EmailMessageDto

--- DealMS/src/main/java/app/gozenko/dto/EmailMessageDto.java
+++ DealMS/src/main/java/app/gozenko/dto/EmailMessageDto.java
@@ -2,6 +2,7 @@ package app.gozenko.dto;
 
 
 import app.gozenko.enums.EmailTheme;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,11 +14,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class EmailMessageDto {
 
+    @Schema(description = "Адрес", example = "mail@gmail.com")
     private String address;
 
+    @Schema(description = "Тема письма", example = "CREDIT_ISSUED")
     private EmailTheme theme;
 
+    @Schema(description = "ID заявления", example = "123e4567-e89b-12d3-a456-426614174000")
     private Long statementId;
 
+    @Schema(description = "Текст письма", example = "Кредит одобрен")
     private String text;
 }
